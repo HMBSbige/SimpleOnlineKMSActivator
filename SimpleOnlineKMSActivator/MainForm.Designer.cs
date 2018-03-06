7841af2eee32dc821bdd8b14ed10aa31d06efb0b
HMBSbige <admin@bige0.com>
2018-03-06 17:02:10 +0800

    Init project

---
+++ SimpleOnlineKMSActivator/MainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace SimpleOnlineKMSActivator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 82);
+            this.textBox1.MaxLength = 0;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(264, 181);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "激活";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 53);
+            this.progressBar1.Maximum = 3;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(183, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(155, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(155, 27);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "KMS 密钥：\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "KMS 服务器：";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 272);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "SimpleOnlineKMSActivator";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SimpleOnlineKMSActivator/MainForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace SimpleOnlineKMSActivator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private static string GetCMDOutput(string cmd)
+        {
+            const string exit = @"exit";
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    FileName = @"cmd.exe",
+                    UseShellExecute = false,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                }
+            };
+            p.Start();
+            p.StandardInput.AutoFlush = true;
+            p.StandardInput.WriteLine(cmd);
+            p.StandardInput.WriteLine(exit);
+            var outStr = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            p.Close();
+
+            var str1 = Application.StartupPath + @">" + cmd + Environment.NewLine;
+            var str2 = Application.StartupPath + @">" + exit + Environment.NewLine;
+            var ret = outStr.Substring(outStr.IndexOf(str1, StringComparison.Ordinal) + str1.Length,
+                outStr.IndexOf(str2, StringComparison.Ordinal) - (outStr.IndexOf(str1, StringComparison.Ordinal) + str1.Length));
+            var ret2 = ret.Trim().Split('\n');
+            return ret2[ret2.Length - 1].Trim();
+        }
+
+        private static string GetWindowsVersion()
+        {
+            const string cmd = @"wmic os get caption";
+            return GetCMDOutput(cmd);
+        }
+
+        private static string GetWindowsLicense()
+        {
+            const string cmd = @"cscript %SystemRoot%\System32\slmgr.vbs /xpr";
+            return GetCMDOutput(cmd);
+        }
+
+        private static string SetLicenseKey(string key)
+        {
+            var cmd = @"cscript %SystemRoot%\System32\slmgr.vbs /ipk " + key;
+            return GetCMDOutput(cmd);
+        }
+
+        private static string SetKMSServer(string serveraddr)
+        {
+            var cmd = @"cscript %SystemRoot%\System32\slmgr.vbs /skms " + serveraddr;
+            return GetCMDOutput(cmd);
+        }
+
+        private static string ActivateWindows()
+        {
+            const string cmd = @"cscript %SystemRoot%\System32\slmgr.vbs /ato";
+            return GetCMDOutput(cmd);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            textBox1.Text = string.Empty;
+            textBox1.Text += GetWindowsVersion() + Environment.NewLine;
+            progressBar1.Value = 0;
+            textBox1.Text += SetLicenseKey(@"W269N-WFGWX-YVC9B-4J6C9-T83GX") + Environment.NewLine;
+            progressBar1.PerformStep();
+            textBox1.Text += SetKMSServer(@"kms.bige0.com") + Environment.NewLine;
+            progressBar1.PerformStep();
+            textBox1.Text += ActivateWindows() + Environment.NewLine;
+            progressBar1.PerformStep();
+            textBox1.Text += GetWindowsLicense() + Environment.NewLine;
+            button1.Enabled = true;
+        }
+    }
+}
